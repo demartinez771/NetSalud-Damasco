07ca29f3a39d86fbd3ab205f0f6dfd3993500b80
CristianSuarezHer <cristianf.suarezh@gmail.com>
2022-09-11 17:41:38 -0500

    first commit

---
+++ NetSalud_be/saludApp/views/__init__.py
@@ -0,0 +1,4 @@
+from .crearUsuarioView import CrearUsuarioView
+from .crearPerSaludView import CrearPersonalSaludView
+from .crearFamiliaView import CrearFamiliarView
+from .crearPacienteView import CrearPacienteView
