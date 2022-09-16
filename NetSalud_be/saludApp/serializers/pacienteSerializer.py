8b94d583be3f0addfbd212a0b61c83ed511f116c
CristianSuarezHer <cristianf.suarezh@gmail.com>
2022-09-15 21:36:15 -0500

    ok

--- NetSalud_be/saludApp/serializers/pacienteSerializer.py
+++ NetSalud_be/saludApp/serializers/pacienteSerializer.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class PacienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Paciente
-        fields = ['documento', 'fecha_nac', 'ciudad', 'direccion', 'username', 'familiar_documento', 'pSalud_documento']
+        fields = ['documento', 'fecha_nac', 'ciudad', 'direccion', 'pSalud_documento', 'username',]
