f94a681b7e2e62a3be00e97996dc4227a1ec92a7
electricl-dev <dsavilav@unal.edu.co>
2022-09-25 13:43:08 -0500

    update css header

--- net_salud_fe/src/App.vue
+++ net_salud_fe/src/App.vue
@@ -1,24 +1,39 @@
-<script>
-
-</script>
+<script></script>
 
 <template>
   <div id="app">
     <div id="header">
-      <img src="./assets/Logo.png" alt="" width="70" height="70">  
-      <div id="slogan">
-        <h1>NetSalud</h1>
-        <h2>La salud a tus manos</h2>
+      <div id="div-logo">
+        <img
+          src="./assets/Logo1.svg"
+          alt=""
+          width="70"
+          height="70"
+          style="filter: opacity(0.7) drop-shadow(0 0 0 #2e2d4d)"
+        />
+        <div id="slogan">
+          <h1>NetSalud</h1>
+          <h2>La salud a tus manos</h2>
+        </div>
       </div>
-      
-        <nav>
-            <button v-if="is_auth" v-on:click="loadHome" > Inicio </button>
-            <button v-if="is_auth" v-on:click="loadPatientRecord"> RegistroPaciente </button>
-            <button v-if="is_auth" v-on:click="loaddoctorRecord"> RegistroMedico </button>
-            <button v-if="is_auth" v-on:click="loadFamilyRecord"> RegistroFamiliar </button>
-            <button v-if="is_auth" v-on:click="loadPatientConsult"> ConsultarPaciente </button>
-            <button v-if="is_auth" v-on:click="logOut"> Cerrar Sesión </button>
-        </nav>
+      <div><h2 id="perfil">Perfil Auxiliar</h2></div>
+      <div><h2 id="c_s">Cerrar Sesión</h2></div>
+      <nav>
+        <button v-if="is_auth" v-on:click="loadHome">Inicio</button>
+        <button v-if="is_auth" v-on:click="loadPatientRecord">
+          RegistroPaciente
+        </button>
+        <button v-if="is_auth" v-on:click="loaddoctorRecord">
+          RegistroMedico
+        </button>
+        <button v-if="is_auth" v-on:click="loadFamilyRecord">
+          RegistroFamiliar
+        </button>
+        <button v-if="is_auth" v-on:click="loadPatientConsult">
+          ConsultarPaciente
+        </button>
+        <button v-if="is_auth" v-on:click="logOut">Cerrar Sesión</button>
+      </nav>
     </div>
   </div>
 </template>
@@ -29,19 +44,43 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: left;
-  color: #2E2D4D;
+  color: #2e2d4d;
   line-height: 0.3;
+  padding: 10px;
 }
-h2{
+
+h2 {
   font-size: 20px;
+  font-family: "Kanit";
 }
+#perfil {
+  width: 800px;
+  height: 56px;
+  font-style: normal;
+  font-weight: 400;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  color: #db5461;
+}
+#c_s {
+  color: #8aa29e;
+  display: flex;
+  height: 56px;
+  align-items: center;
+}
+
 #header {
   flex-direction: row !important;
   display: flex !important;
+  justify-content: space-between;
 }
-#slogan{
+#div-logo {
+  flex-direction: row !important;
+  display: flex !important;
+}
+#slogan {
   margin-left: 10px;
-  background-color: #42b983;
 }
 nav {
   padding: 30px;
